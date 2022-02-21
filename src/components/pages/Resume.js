1d432a3dc12f3e73796301ec7120d5d236fbd3c8
DanielWestiner <daniel.westiner@gmail.com>
2022-02-21 11:36:01 -0500

    fixed some stlying in resume and updated layout functionality

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,12 +3,12 @@ import '../../styles/Resume.css'
 
 export default function Resume() {
   return (
-    <div className='res-page container-fluid'>
-      <div className='row'>
-        <div className='col'>
+    <div className='res-page container'>
+      <div className='row res-area'>
+        <div className='col-sm-6 res-skills'>
           <h2 className='res-intro'>A Little About Myself</h2>
           <div className='row'>
-            <p className='res-intro'>
+            <p className='res-intro col'>
               A Philadelphia-based full-stack web developer with skills in:
             </p>
             <ul className='res-intro'>
@@ -23,16 +23,16 @@ export default function Resume() {
             </ul>
           </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col text-center res-info'>
-        <h5>Take a peek at my resume here:</h5>
-          <a href='https://drive.google.com/file/d/1q_K0nXou99Rz_sE6qO967IRMuCxS2VyU/view?usp=sharing' className='btn btn-light res-btn'> My Resume</a>
-
+        <div className='col-sm-6 res-link'>
+          <div className='col text-center res-info'>
+            <h5>Take a peek at my resume here:</h5>
+            <a href='https://drive.google.com/file/d/1q_K0nXou99Rz_sE6qO967IRMuCxS2VyU/view?usp=sharing' className='btn btn-light res-btn'> My Resume</a>
+          </div>
         </div>
       </div>
-      
-      <div className='res-intro-bg col offset-md-6'>
+
+
+      <div className='res-intro-bg col-sm-12 offset-sm-12'>
 
       </div>
     </div>
